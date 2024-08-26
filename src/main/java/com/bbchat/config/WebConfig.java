1e121acbc498284e13ee367d4628a797c8b0d7bd
yongchann <yongcgp@naver.com>
2024-08-26 15:34:20 +0900

    add: cors 처리

---
+++ src/main/java/com/bbchat/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.bbchat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Authorization", "Content-Type")
+//                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
