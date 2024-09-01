3bf8517678a83073ae88c3000ac4fd81e04c9595
yongchann <yongcgp@naver.com>
2024-09-02 00:35:52 +0900

    feat: 토큰 기반 인증, 인터셉터를 통한 인가 처리 구현

--- src/main/java/com/bbchat/config/WebConfig.java
+++ src/main/java/com/bbchat/config/WebConfig.java
@@ -1,11 +1,16 @@
 package com.bbchat.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -15,5 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Authorization", "Content-Type")
 //                .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/v1/login", "/api/v1/logout");
     }
 }
---
+++ src/main/java/com/bbchat/controller/v1/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.bbchat.controller.v1.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginRequest {
+
+    private String username;
+
+    private String password;
+}
