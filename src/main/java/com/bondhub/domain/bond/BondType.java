84453ce61460f71fbc1c125adfdad1e700e1c17c
yongchann <yongcgp@naver.com>
2024-10-11 06:38:14 +0900

    fix: JPA ENUM 업데이트를 위한 임시 추가

--- src/main/java/com/bondhub/domain/bond/BondType.java
+++ src/main/java/com/bondhub/domain/bond/BondType.java
@@ -6,7 +6,9 @@ public enum BondType {
     SPECIAL_BANK("특수은행채"),
     CARD("카드채"),
     CAPITAL("캐피탈채"),
-    COMPANY("회사채");
+    COMPANY("회사채"),
+    BANK("은행채"),
+    SPECIALIZED_CREDIT("여전채");
 
     BondType(String name) {
     }
