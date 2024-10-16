2851e15a978889312b5469e8f827388c59cabf20
yongchann <yongcgp@naver.com>
2024-10-17 01:33:57 +0900

    remove: 채팅 파일기반 전체분석 API 전체 코드 제거

--- src/main/java/com/bondhub/service/ChatService.java
+++ src/main/java/com/bondhub/service/ChatService.java
@@ -2,14 +2,12 @@ package com.bondhub.service;
 
 import com.bondhub.domain.ask.Bond;
 import com.bondhub.domain.chat.*;
-import com.bondhub.domain.common.FileInfo;
 import com.bondhub.service.analysis.ChatAnalyzer;
 import com.bondhub.service.analysis.ChatParser;
 import com.bondhub.service.dto.BondChatDto;
 import com.bondhub.service.dto.ChatDto;
 import com.bondhub.service.dto.ChatGroupByContentDto;
 import com.bondhub.service.exception.NotFoundAggregationException;
-import com.bondhub.support.S3FileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,12 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static com.bondhub.service.UploadService.CHAT_FILE_KEY_PREFIX;
-import static com.bondhub.service.UploadService.CHAT_FILE_SAVE_NAME;
 import static com.bondhub.service.analysis.ChatParser.CHAT_SPLIT_DELIMITER;
-import static com.bondhub.support.S3FileRepository.buildPath;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,14 +26,10 @@ public class ChatService {
     private final ChatParser chatParser;
     private final ChatAnalyzer chatAnalyzer;
     private final ChatAppender chatAppender;
-
     private final ChatFinder chatFinder;
-    private final ChatRemover chatRemover;
-    private final ChatRepository  chatRepository;
     private final ChatAggregationRepository chatAggregationRepository;
     private final MultiBondChatHistoryRepository multiBondChatHistoryRepository;
 
-    private final S3FileRepository fileRepository;
 
     public List<ChatGroupByContentDto> getChatsGroupByContent(String chatDate, ChatStatus status) {
         List<Chat> chats = chatFinder.findDailyByStatus(chatDate, status);
@@ -72,7 +62,7 @@ public class ChatService {
         // 개별 채팅에 대해 분류 및 기존 채팅 상태 변경
         multiBondChats.forEach(chat ->  chat.setStatus(ChatStatus.SEPARATED));
         separatedChats.forEach(chatAnalyzer::analyze);
-        chatRepository.saveAll(separatedChats);
+        chatAppender.append(separatedChats);
 
         // 분리 이력 추가
         String joinedContents = String.join(CHAT_SPLIT_DELIMITER, separatedChats.stream().map(Chat::getContent).toList());
@@ -168,39 +158,4 @@ public class ChatService {
         return groupByBond(successChats);
     }
 
-    @Transactional
-    public void reanalyzeAll(String date) {
-        // 홰당 일자의 채팅을 모두 삭제
-        int deletedCount = chatRemover.removeDailyAll(date);
-        log.info("[aggregateChat] deleted {} chats", deletedCount);
-
-        // 3개의 채팅 데이터 조회 후 하나의 문자열로 병합
-        String entireChatStr = Stream.of("BB", "RB", "MM")
-                .map(type -> {
-                    FileInfo chatFile = fileRepository.get(buildPath(CHAT_FILE_KEY_PREFIX, date, type), CHAT_FILE_SAVE_NAME);
-                    return chatParser.preprocess(chatFile.getContent());
-                })
-                .collect(Collectors.joining());
-
-        // 채팅 가공 후 모두 저장
-        List<Chat> allChats = chatParser.parseChatStr(date, entireChatStr);
-        allChats.forEach(chatAnalyzer::analyze);
-        chatRepository.saveAll(allChats); // TODO JDBC Batch insert 적용
-        log.info("[aggregateChat] created {} chats", allChats.size());
-
-        // 채팅 가공 상태에 따른 집계 결과를 생성
-        Map<ChatStatus, Long> statusCounts = allChats.stream()
-                .collect(Collectors.groupingBy(Chat::getStatus, Collectors.counting()));
-
-        ChatAggregation aggregation = ChatAggregation.builder()
-                .chatDate(date)
-                .totalChatCount(allChats.size())
-                .notUsedChatCount(statusCounts.getOrDefault(ChatStatus.CREATED, 0L))
-                .multiDueDateChatCount(statusCounts.getOrDefault(ChatStatus.NEEDS_SEPARATION, 0L))
-                .uncategorizedChatCount(statusCounts.getOrDefault(ChatStatus.UNCATEGORIZED, 0L))
-                .fullyProcessedChatCount(statusCounts.getOrDefault(ChatStatus.OK, 0L))
-                .build();
-
-        chatAggregationRepository.save(aggregation);
-    }
 }
--- src/main/java/com/bondhub/service/analysis/ChatParser.java
+++ src/main/java/com/bondhub/service/analysis/ChatParser.java
@@ -1,65 +1,22 @@
 package com.bondhub.service.analysis;
 
 import com.bondhub.domain.chat.Chat;
-import com.bondhub.domain.chat.ChatStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class ChatParser {
 
-    private static final String CHAT_MESSAGE_SEARCH_PATTERN = "([A-Za-z\\.가-힣0-9 女]+) \\((\\d{2}:\\d{2}:\\d{2})\\) :\\s*(.*?)(?=(?:[A-Z0-9a-z\\.가-힣 女]+\\s\\(\\d{2}:\\d{2}:\\d{2}\\)|$))";
-    private static final String SENDER_ADDRESS_PATTERN = "\\s*\\([^)]*\\)\\s*$|\\s*\\[[^]]*\\]\\s*$|\\s*\\{[^}]*\\}\\s*$|\\s*<[^>]*>\\s*$|\\s*▨[^▨]*▨\\s*$|\\s*【[^】]*】\\s*$";
     public final static String CHAT_SPLIT_DELIMITER = "§";
-
-    private static final Map<String, String> REPLACEMENT_RULES = Map.of(
-            "[부국채영]368-9532", "([부국채영]368-9532])",
-            "(DS투자증권 채권전략팀 02)709-2701)", "(DS투자증권 채권전략팀 02-709-2701)",
-            "[흥국채금 6260-2460)", "[흥국채금 6260-2460]",
-            "김성훈(부국)", "김성훈부국",
-            "\r\n", " ");
-
     private final MaturityDateExtractor maturityDateExtractor;
-
-    public List<Chat> parseChatStr(String chatDate, String rawContentChat) {
-        Pattern pattern = Pattern.compile(CHAT_MESSAGE_SEARCH_PATTERN);
-        Matcher matcher = pattern.matcher(rawContentChat);
-
-        List<Chat> chats = new ArrayList<>();
-        while (matcher.find()) {
-            String senderName = matcher.group(1).trim();
-            String sendTime = matcher.group(2);
-            String content = matcher.group(3);
-            String senderAddress = extractSenderAddress(content).trim();
-            content = content.replace(senderAddress, "").trim();
-            chats.add(Chat.builder()
-                    .chatDateTime(LocalDateTime.of(LocalDate.parse(chatDate), LocalTime.parse(sendTime)))
-                    .senderName(senderName)
-                    .content(content)
-                    .senderAddress(senderAddress)
-                    .status(ChatStatus.CREATED)
-                    .maturityDate("")
-                    .build());
-        }
-
-        return removeDuplication(chats);
-    }
-
-    public String extractSenderAddress(String content) {
-        Pattern pattern = Pattern.compile(SENDER_ADDRESS_PATTERN);
-        Matcher matcher = pattern.matcher(content);
-        return matcher.find() ? matcher.group(0).trim() : "";
-    }
 
     public List<Chat> parseMultiBondChat(Chat multiBondChat, List<String> singleBondContents) {
         List<Chat> singleBondChats = new ArrayList<>();
@@ -80,17 +37,6 @@ public class ChatParser {
 
         return singleBondChats;
     }
-
-    public String preprocess(String rawText) {
-        int index = rawText.indexOf("\r\n");
-        rawText = rawText.substring(index + "\r\n".length());
-        for (Map.Entry<String, String> entry : REPLACEMENT_RULES.entrySet()) {
-            rawText = rawText.replace(entry.getKey(), entry.getValue());
-        }
-
-        return rawText;
-    }
-
 
     public List<Chat> removeDuplication(List<Chat> allChats) {
         Map<String, Chat> uniqueChats = new HashMap<>();
